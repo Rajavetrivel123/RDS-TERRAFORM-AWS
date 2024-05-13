f7f31ddbd6b9d92c709713689751970d9fe0763e
manikandan <rajavetrivelmani@gmail.com>
2024-05-13 12:32:52 +0530

    terraform module for rds and jenkins cicd

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform.s3.22-04-24"
+    key            = "jenkins"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraforms"
+  }
+}
